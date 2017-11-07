1c3b930022c139e1a07e1906ce45b81d91ff75b6
sanadhis <sanadhis@gmail.com>
2017-11-07 14:17:49 +0100

    Tidy up install scripts

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-PLAYBOOK="prerequisites.yml"
-current_dir="$PWD"
+set -e
 
-NODES=$(kubectl get nodes -o json | jq '.items[] | .status .addresses[] | select(.type=="InternalIP") | .address')
+function get-hosts () {
+    nodes=$(kubectl get nodes -o json | jq '.items[] | .status .addresses[] | select(.type=="InternalIP") | .address')
 
-mkdir -p inventory
-touch inventory/target
-echo "[kube-cluster]" > inventory/target
+    mkdir -p inventory
+    touch inventory/target
+    echo "[kube-cluster]" > inventory/target
 
-for node in $NODES
-do
-    echo $node >> inventory/target
-done
+    for node in $nodes
+    do
+        echo $node >> inventory/target
+    done
+}
 
-ansible-playbook playbooks/$PLAYBOOK \
-  -i $PWD/inventory/target \
-  -e "target=kube-cluster" \
-  -e "current_dir=${current_dir}"
+function main () {
+    PLAYBOOK="prerequisites.yml"
+    current_dir="$PWD"
+
+    ansible-playbook playbooks/$PLAYBOOK \
+      -i $PWD/inventory/target \
+      -e "target=kube-cluster" \
+      -e "current_dir=${current_dir}"
+}
+
+get-hosts
+main "$@"
