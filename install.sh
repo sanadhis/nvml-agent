dd8ee4d554ffd6681a1aa41595633783376e0dc5
sanadhis <sanadhis@gmail.com>
2017-11-15 23:31:46 +0100

    Update Install.sh, ask credentials

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+function print-banner () {
+    local message="$1"
+    echo "##################################"
+    echo "$message"
+    echo "##################################"
+}
+
 function get-hosts () {
     nodes=$(kubectl get nodes -o json | jq '.items[] | .status .addresses[] | select(.type=="InternalIP") | .address')
 
@@ -19,10 +26,27 @@ function main () {
     PLAYBOOK="nvml-agent.yml"
     current_dir="$PWD"
 
+    print-banner "Input hostname address and port for influxdb server"
+    read -p "Enter HOSTNAME": influxdb_host
+    read -p "Enter PORT(default:8086)": influxdb_port
+
+    print-banner "Input username and password access to write into influxdb server"
+    read -p "Enter USERNAME(default:root)": influxdb_user
+    read -p "Enter PASSWORD(default:root)": influxdb_pass
+
+    print-banner "Input database to write the statistics for"
+    read -p "Enter db name": influxdb_db   
+
     ansible-playbook playbooks/$PLAYBOOK -K \
       -i $PWD/inventory/target \
       -e "target=kube-cluster" \
-      -e "current_dir=${current_dir}"
+      -e "current_dir=${current_dir}" \
+      -e "influxdb_host=$influxdb_host" \
+      -e "influxdb_port=$influxdb_port" \
+      -e "influxdb_user=$influxdb_user" \
+      -e "influxdb_pass=$influxdb_pass" \
+      -e "influxdb_db=$influxdb_db"
+      
 }
 
 get-hosts
